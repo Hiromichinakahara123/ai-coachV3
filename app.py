3889d0779cf47bf3215223ba77507d8a5ee7afa8
Hiromichinakahara123 <nakahara@daiichi-cps.ac.jp>
2026-01-08 14:24:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ EXPECTED_COLUMNS = [
 # =====================================================
 # DB (SQLite)
 # =====================================================
-@st.cache_resource
+#@st.cache_resource
 def get_conn():
     conn = sqlite3.connect(DB_PATH, check_same_thread=False)
     conn.row_factory = sqlite3.Row
@@ -706,6 +706,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
